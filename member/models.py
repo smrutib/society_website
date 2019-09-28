1dd42966a1f5a7b6a047fee608c1c13e62b25cd5
pragnyak <pragnyak09@gmail.com>
2019-09-28 22:27:04 +0530

    forms.py

--- member/models.py
+++ member/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+#from home.models import CustomUser
 
 # Create your models here.
+class Request(models.Model):
+
+	request_date=models.DateField( auto_now_add=False)
+	flatno=models.CharField(max_length=5)
+	username=models.CharField(max_length=200)
+	#username= models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+	request_choices = [
+        ('NOC', 'NOC'),
+        ('AddressProof', 'AddressProof'),
+        ('SalesAgreement', 'SalesAgreement'),
+        ('CCTV', 'CCTv'),
+        ('Other','Other')
+        ]
+	request_type=models.CharField(
+        max_length=100,
+        choices=request_choices,
+        default='NOC',
+        )
+	name_to_be_addressed=models.CharField(max_length=100)
+	new_members_names=models.CharField(max_length=200)
+	sales_details=models.CharField(max_length=300)
+	cctv_from=models.DateTimeField()
+	cctv_to=models.DateTimeField()
